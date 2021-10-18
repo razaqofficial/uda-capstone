05935fb5c02a007dc52f875139187c3f36a472ea
abdurrazaq <razaqofficial@gmail.com>
2021-10-18 20:42:54 +0100

    upated config

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 const customerRoutes = require('./routes/customer');
 
 // settings
-app.set('port', process.env.APP_PORT || 3001);
+app.set('port', process.env.APP_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
